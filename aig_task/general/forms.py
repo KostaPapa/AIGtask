610f89d80aa45b43b89dc213646da0acb8cba495
KostaPapa <kosta2pa@yahoo.com>
2015-04-26 21:09:00 -0400

    created forms

---
+++ aig_task/general/forms.py
@@ -0,0 +1,47 @@
+"""
+<Program Name>
+  forms.py
+
+<Author>
+  Kostaq Papa <kp967@nyu.edu>
+
+<Started>
+  April 26, 2015 
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  It's where the django documentation recommends placing all forms code. 
+  Keeps code easily maintainable.
+"""
+
+from django import forms
+from .models import Join
+
+class EmailForm(forms.Form):
+  """
+  <Purpose>
+    Allows a user to join by typing their email address in the form.
+      
+  <Arguments>
+    forms.Form:
+      Each form is a Python class that subclasses django.forms.Form.
+
+  <Exceptions>
+    None.
+
+  <Side Effects>
+    None.
+
+  <Returns>
+    None.
+  """
+  
+  username = forms.CharField(required=False)
+  email = forms.EmailField()
+
+
+class JoinForm(forms.ModelForm):
+  class Meta:
+    model = Join
--- aig_task/general/views.py
+++ aig_task/general/views.py
@@ -1,11 +1,48 @@
+"""
+<Program Name>
+  views.py
+
+<Author>
+  Kostaq Papa <kp967@nyu.edu>
+
+<Started>
+  April 26, 2015 
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  A view is a Python function that takes a Web request and returns
+  a Web response. This response can be the HTML contents of a Web page, 
+  or a redirect, or a 404 error, or an XML document, or an image, etc. 
+  The view itself contains whatever arbitrary logic is necessary to return 
+  that response.
+"""
+
 from django.shortcuts import render
-
-
-
-
+from .forms import EmailForm, JoinForm
+from .models import Join
 
 def home(request):
-	context = {}
+	
+
+
+	# form = EmailForm(request.POST or None)
+	
+	# if form.is_valid():
+	# 	email = (form.cleaned_data['email'])
+	# 	new_join, created = Join.objects.get_or_create(email=email)
+	# 	print (new_join, created)
+	# 	print (new_join.timestamp)
+
+	form = JoinForm(request.POST or None)
+	if form.is_valid():
+		new_join = form.save(commit=False)
+		email = (form.cleaned_data['email'])
+		new_join_old, created = Join.objects.get_or_create(email=email)
+		# new_join.save()
+
+	context = {"form": form}
 	template = "home.html"
 
 	return render(request, template, context)
